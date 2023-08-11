5ebcdb719f7f000f0c1ac96d672999999740758e
luanphan2911 <baoluan291103oe@gmail.com>
2023-08-11 19:42:48 +0700

    update readme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import "./App.css";
 import GenerateColor from "./components/GenerateColor";
-
-function App() {
-  return (
-    <div className="App">
-      <GenerateColor />
-    </div>
-  );
+import React from "react";
+class App extends React.Component {
+  state = {
+    show: true,
+  };
+  componentDidMount() {
+    console.log("mount");
+  }
+  componentDidUpdate() {
+    console.log("update");
+  }
+  componentWillUnmount() {
+    console.log("unmount");
+  }
+  render() {
+    let { show } = this.state;
+    return (
+      <div className="App">
+        <button onClick={() => this.setState({ ...this.state, show: !show })}>
+          Toggle
+        </button>
+        {show && <GenerateColor />}
+      </div>
+    );
+  }
 }
 
 export default App;
